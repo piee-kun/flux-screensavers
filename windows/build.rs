7cea261e6d88905fea413298c811664355c73c8b
sandydoo <hey@sandydoo.me>
2022-05-21 03:06:11 +0400

    Add a note why windres isn’t working

--- windows/build.rs
+++ windows/build.rs
@@ -6,7 +6,9 @@ use std::error::Error;
 #[cfg(windows)]
 fn main() -> Result<(), Box<dyn Error>> {
     let mut resource = winres::WindowsResource::new();
-    resource.set_icon("flux.ico");
+    // This doesn’t work right now because NixOS doesn’t have windres. Or I
+    // can’t find it...
+    // resource.set_icon("flux.ico"); // TODO: add an icon
     resource.set_manifest_file("flux-windows-screensaver.exe.manifest");
     resource.compile()?;
 
