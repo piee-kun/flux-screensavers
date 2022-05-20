937d8547e7bb2ee0bd3148bc5be1ef3060090521
sandydoo <hey@sandydoo.me>
2022-05-20 19:02:43 +0400

    Fix build scripts

--- windows/build.rs
+++ windows/build.rs
@@ -1,13 +1,17 @@
+#[cfg(windows)]
+extern crate winres;
+#[cfg(windows)]
 use std::error::Error;
 
+#[cfg(windows)]
 fn main() -> Result<(), Box<dyn Error>> {
-    #[cfg(windows)]
-    {
-        let mut resource = winres::WindowsResource::new();
-        resource.set_icon("flux.ico");
-        resource.set_manifest_file("flux-windows-screensaver.exe.manifest");
-        resource.compile().unwrap();
-    }
+    let mut resource = winres::WindowsResource::new();
+    resource.set_icon("flux.ico");
+    resource.set_manifest_file("flux-windows-screensaver.exe.manifest");
+    resource.compile()?;
 
     Ok(())
 }
+
+#[cfg(unix)]
+fn main() {}
