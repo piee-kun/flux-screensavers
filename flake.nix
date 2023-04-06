9c30db5b7ad996987cb2a403c20e9ccb99cb5aa1
sandydoo <hey@sandydoo.me>
2023-04-06 04:23:17 +0000

    Switch nixpkgs to 23.05-pre
    mingw64 is broken on stable right now.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flux Screensavers";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/23.05-pre";
     flake-utils.url = "github:numtide/flake-utils";
     crane = {
       url = "github:ipetkov/crane";
